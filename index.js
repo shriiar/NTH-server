04e2b0ed8a16279769b6391545a09488c4cf0916
Sajid Shahariar <fahimshariar511@gmail.com>
2022-05-16 02:09:05 +0600

    whole lotta updates

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
         const subjectsCollection = client.db('NTH').collection('Subjects');
         const studentsCollection = client.db('NTH').collection('Students');
         const subWAccCollection = client.db('NTH').collection('SubWAcc');
+        const noticeCollection = client.db('NTH').collection('Notice');
 
         app.post('/login', async (req, res) => {
             const user = req.body;
@@ -93,6 +94,47 @@ async function run() {
             }
         })
 
+        app.get('/notice', verifyJWT, async (req, res) => {
+            const className = req.query.className;
+            const batch = req.query.batch;
+            const group = req.query.group;
+            console.log(className, batch, group);
+            const query = { className: className, batch: batch, group: group };
+            const cursor = noticeCollection.find(query);
+            const subjects = await cursor.toArray();
+            res.send(subjects);
+        })
+
+        app.get('/notice/admin', verifyJWT, async (req, res) => {
+            const cursor = noticeCollection.find();
+            const notice = await cursor.toArray();
+            res.send(notice);
+        })
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await studentsCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/students/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await studentsCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await studentsCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        })
+
         app.post('/students', async (req, res) => {
             const student = req.body;
             const query = { name: student.name, father: student.father, mother: student.mother, className: student.className, id: student.id, batch: student.batch, group: student.group };
@@ -102,10 +144,6 @@ async function run() {
             }
             const result = await studentsCollection.insertOne(student);
             res.send({ success: true, result });
-
-            // const newCar = req.body;
-            // const result = await carsCollection.insertOne(newCar);
-            // res.send(result);
         })
 
         app.post('/subWAcc', async (req, res) => {
@@ -117,10 +155,22 @@ async function run() {
             }
             const result = await subWAccCollection.insertOne(item);
             res.send({ success: true, result });
+        })
 
-            // const newCar = req.body;
-            // const result = await carsCollection.insertOne(newCar);
-            // res.send(result);
+        app.post('/notice', async (req, res) => {
+            const notice = req.body;
+            const result = await noticeCollection.insertOne(notice);
+            res.send(result);
+        })
+
+        app.delete('/notice', async (req, res) => {
+            const className = req.query.className;
+            const batch = req.query.batch;
+            const group = req.query.group;
+            console.log(className, batch, group);
+            const query = { className: className, batch: batch, group: group };
+            const result = await noticeCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {

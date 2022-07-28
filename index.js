30c2d0a1c6200edb96f0689988b709f5e7c2c97f
Sajid Shahariar <fahimshariar511@gmail.com>
2022-07-29 00:07:44 +0600

    ok

--- index.js
+++ index.js
@@ -131,12 +131,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 function verifyJWT(req, res, next) {
 	const authHeader = req.headers.authorization;
 	if (!authHeader) {
-		return res.status(401).send({ message: 'unauthorized access' });
+		return;
 	}
 	const token = authHeader.split(' ')[1];
 	jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
 		if (err) {
-			return res.status(403).send({ message: 'Forbidden access' });
+			return;
 		}
 		// console.log('decoded', decoded);
 		req.decoded = decoded;
@@ -380,6 +380,7 @@ async function run() {
 					payYear: updatedUser.payYear
 				}
 			};
+			console.log(updateDoc);
 			const result = await studentsCollection.updateOne(filter, updateDoc, options);
 			res.send(result);
 		})

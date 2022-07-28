8752559e262980ba9d4f297136a89cd279856a1b
Sajid Shahariar <fahimshariar511@gmail.com>
2022-07-29 00:10:22 +0600

    ok

--- index.js
+++ index.js
@@ -131,12 +131,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 function verifyJWT(req, res, next) {
 	const authHeader = req.headers.authorization;
 	if (!authHeader) {
-		return;
+		return res.status(401).send({ message: 'unauthorized access' });
 	}
 	const token = authHeader.split(' ')[1];
 	jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
 		if (err) {
-			return;
+			return res.status(403).send({ message: 'Forbidden access' });
 		}
 		// console.log('decoded', decoded);
 		req.decoded = decoded;
